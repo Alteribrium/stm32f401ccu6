661cfc0ecffa2aa74e71e30e095e590489d2bca8
Dzmitry <ya@gmail.com>
2025-12-07 18:31:15 +0300

    sth

--- Code/USART/USART.c
+++ Code/USART/USART.c
@@ -1,13 +1,19 @@
 #include "USART.h"
 #include <stdio.h>
+#include <string.h>
 char temp = 0;
 
+void USART2_IRQHandler(void);
+
+extern uint16_t buff_rx_len;
+uint16_t buff_rx_len = 0;
 static uint8_t dma_tx_buffer[256];
 static uint8_t dma_rx_buffer[256];
 static uint8_t dma_tx_busy = 0;
-static uint8_t dma_rx_ready = 0;
-static uint32_t dma_rx_length = 0; 
-static uint16_t len;
+extern uint8_t dma_rx_ready;
+uint8_t dma_rx_ready = 0;
+//static uint32_t dma_rx_length = 0; 
+//static uint16_t len;
 
 void USART2_init(void){
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
@@ -24,8 +30,8 @@ void USART2_init(void){
 	
 	USART2_DMA_init();
 	
-	//USART2->CR1 |= USART_CR1_RXNEIE;
-	//NVIC_EnableIRQ(USART2_IRQn);
+	USART2->CR1 |= USART_CR1_IDLEIE;
+	NVIC_EnableIRQ(USART2_IRQn);
 }
 
 void USART2_DMA_init(void){
@@ -36,19 +42,14 @@ void USART2_DMA_init(void){
     DMA1_Stream5->NDTR = sizeof(dma_rx_buffer);
     
     DMA1_Stream5->CR = (1 << 27) |        // Channel 4
-                      DMA_SxCR_MINC |    // Memory increment
-                      DMA_SxCR_TCIE;     // Transfer complete interrupt
-    
-    USART2->CR3 |= USART_CR3_DMAR;
+                      DMA_SxCR_MINC ;   // Memory increment
+                      //DMA_SxCR_TCIE;     // Transfer complete interrupt
     
     DMA1_Stream6->PAR = (uint32_t)&USART2->DR;
     DMA1_Stream6->M0AR = (uint32_t)dma_tx_buffer;
     DMA1_Stream6->NDTR = 0;
     
-    DMA1_Stream6->CR = (1 << 27) |   // Channel 4 (USART2_TX)
-                      DMA_SxCR_PL_0 |
-                      DMA_SxCR_MSIZE_0 |
-                      DMA_SxCR_PSIZE_0 |
+    DMA1_Stream6->CR = (1 << 27) |   // Channel 4 (USART2_TX) //4 << DMA_SxCR_CHSEL_Pos
                       DMA_SxCR_MINC |
                       DMA_SxCR_DIR_0 |      // Memory to peripheral
                       DMA_SxCR_TCIE;        // Transfer complete interrupt
@@ -56,7 +57,7 @@ void USART2_DMA_init(void){
     USART2->CR3 |= USART_CR3_DMAT | USART_CR3_DMAR;
     
     NVIC_EnableIRQ(DMA1_Stream6_IRQn); // TX
-    NVIC_EnableIRQ(DMA1_Stream5_IRQn); // RX
+   // NVIC_EnableIRQ(DMA1_Stream5_IRQn); // RX
     
     DMA1_Stream5->CR |= DMA_SxCR_EN;
 }
@@ -76,21 +77,24 @@ void USART2_DMA_SendChar(char str){
     DMA1_Stream6->CR |= DMA_SxCR_EN;
 }
 
-void USART2_DMA_SendString(char *str){
-    while(dma_tx_busy);
+void USART2_DMA_SendString(char *str,uint16_t l){
+		uint64_t startTime = msCounter;
+		while(dma_tx_busy) {
+			if(msCounter - startTime > 1000)
+				break;
+		}
 		dma_tx_busy = 1;
-    
-    len = 0;
-    while(str[len] != '\0'){
+		memcpy(dma_tx_buffer , str, l); 
+    /*while(str[len] != '\0'){
         dma_tx_buffer[len] = str[len];
         len++;
     }
-    
+    */
     DMA1_Stream6->CR &= ~DMA_SxCR_EN;
     while(DMA1_Stream6->CR & DMA_SxCR_EN);
     
     DMA1_Stream6->M0AR = (uint32_t)dma_tx_buffer;
-    DMA1_Stream6->NDTR = len;
+    DMA1_Stream6->NDTR = l;
     
     
     DMA1_Stream6->CR |= DMA_SxCR_EN;
@@ -102,23 +106,19 @@ void DMA1_Stream6_IRQHandler(void){
         dma_tx_busy = 0;
     }
 }
-/*
-void DMA1_Stream5_IRQHandler(void){
-    if(DMA1->LISR & (1 << 11)){  // TCIF5 - Transfer Complete
-        DMA1->LIFCR |= (1 << 11); // Clear TCIF5
-        
-        dma_rx_length = sizeof(dma_rx_buffer);
-        dma_rx_ready = 1;
-        
-        DMA1_Stream5->CR &= ~DMA_SxCR_EN;
-        while(DMA1_Stream5->CR & DMA_SxCR_EN);
-        
-        DMA1_Stream5->NDTR = sizeof(dma_rx_buffer);
-        DMA1_Stream5->CR |= DMA_SxCR_EN;
-			
-				USART2_Process_Received_Data();
-    }
+void USART2_IRQHandler(void){
+	USART2->SR &= ~USART_SR_IDLE;
+	volatile uint32_t temp = USART2->DR;
+	temp = USART2->SR;
+	(void)temp;
+	buff_rx_len = 256 - (uint16_t)DMA1_Stream5->NDTR;
+	USART2_DMA_SendString((char *)(dma_rx_buffer), buff_rx_len);
+	DMA1_Stream5->CR &= ~DMA_SxCR_EN;
+	DMA1->HIFCR = DMA_HIFCR_CTCIF5;
+	DMA1_Stream5->NDTR = 256;
+	DMA1_Stream5->CR |= DMA_SxCR_EN;
 }
+/*
 
 void USART2_Process_Received_Data(void){
 	 
